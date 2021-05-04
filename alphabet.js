ee6a23ee109f3d7e08d6ff1997cdbfb3648db680
ArudoPE <aortizparodi@outlook.com>
2021-05-03 23:38:32 -0500

    Improve to js and css

--- alphabet.js
+++ alphabet.js
@@ -1,109 +1,83 @@
 //LOADING SOUNDS
-var aSound;
-aSound = new sound("./sounds/A.wav");
-var bSound;
-bSound = new sound("./sounds/B.wav");
-var cSound;
-cSound = new sound("./sounds/C.wav");
-var dSound;
-dSound = new sound("./sounds/D.wav");
-var eSound;
-eSound = new sound("./sounds/E.wav");
-var fSound;
-fSound = new sound("./sounds/F.wav");
-var gSound;
-gSound = new sound("./sounds/G.wav");
-var hSound;
-hSound = new sound("./sounds/H.wav");
-var iSound;
-iSound = new sound("./sounds/I.wav");
-var jSound;
-jSound = new sound("./sounds/J.wav");
-var kSound;
-kSound = new sound("./sounds/K.wav");
-var lSound;
-lSound = new sound("./sounds/L.wav");
-var mSound;
-mSound = new sound("./sounds/M.wav");
-var nSound;
-nSound = new sound("./sounds/N.wav");
-var oSound;
-oSound = new sound("./sounds/O.wav");
-var pSound;
-pSound = new sound("./sounds/P.wav");
-var qSound;
-qSound = new sound("./sounds/Q.wav");
-var rSound;
-rSound = new sound("./sounds/R.wav");
-var sSound;
-sSound = new sound("./sounds/S.wav");
-var tSound;
-tSound = new sound("./sounds/T.wav");
-var uSound;
-uSound = new sound("./sounds/U.wav");
-var vSound;
-vSound = new sound("./sounds/V.wav");
-var wSound;
-wSound = new sound("./sounds/W.wav");
-var xSound;
-xSound = new sound("./sounds/X.wav");
-var ySound;
-ySound = new sound("./sounds/Y.wav");
-var zSound;
-zSound = new sound("./sounds/Z.wav");
+let aSound = new sound("./sounds/A.wav");
+let bSound = new sound("./sounds/B.wav");
+let cSound = new sound("./sounds/C.wav");
+let dSound = new sound("./sounds/D.wav");
+let eSound = new sound("./sounds/E.wav");
+let fSound = new sound("./sounds/F.wav");
+let gSound = new sound("./sounds/G.wav");
+let hSound = new sound("./sounds/H.wav");
+let iSound = new sound("./sounds/I.wav");
+let jSound = new sound("./sounds/J.wav");
+let kSound = new sound("./sounds/K.wav");
+let lSound = new sound("./sounds/L.wav");
+let mSound = new sound("./sounds/M.wav");
+let nSound = new sound("./sounds/N.wav");
+let oSound = new sound("./sounds/O.wav");
+let pSound = new sound("./sounds/P.wav");
+let qSound = new sound("./sounds/Q.wav");
+let rSound = new sound("./sounds/R.wav");
+let sSound = new sound("./sounds/S.wav");
+let tSound = new sound("./sounds/T.wav");
+let uSound = new sound("./sounds/U.wav");
+let vSound = new sound("./sounds/V.wav");
+let wSound = new sound("./sounds/W.wav");
+let xSound = new sound("./sounds/X.wav");
+let ySound = new sound("./sounds/Y.wav");
+let zSound = new sound("./sounds/Z.wav");
 
 //GETTING THE TOUCH EVENT
-var letterA = document.getElementById("letterA");
+let letterA = document.getElementById("letterA");
 letterA.addEventListener("touchstart", playSound);
-var letterB = document.getElementById("letterB");
+let letterB = document.getElementById("letterB");
 letterB.addEventListener("touchstart", playSound);
-var letterC = document.getElementById("letterC");
+let letterC = document.getElementById("letterC");
 letterC.addEventListener("touchstart", playSound);
-var letterD = document.getElementById("letterD");
+let letterD = document.getElementById("letterD");
 letterD.addEventListener("touchstart", playSound);
-var letterE = document.getElementById("letterE");
+let letterE = document.getElementById("letterE");
 letterE.addEventListener("touchstart", playSound);
-var letterF = document.getElementById("letterF");
+let letterF = document.getElementById("letterF");
 letterF.addEventListener("touchstart", playSound);
-var letterG = document.getElementById("letterG");
+let letterG = document.getElementById("letterG");
 letterG.addEventListener("touchstart", playSound);
-var letterH = document.getElementById("letterH");
+let letterH = document.getElementById("letterH");
 letterH.addEventListener("touchstart", playSound);
-var letterI = document.getElementById("letterI");
+let letterI = document.getElementById("letterI");
 letterI.addEventListener("touchstart", playSound);
-var letterJ = document.getElementById("letterJ");
+let letterJ = document.getElementById("letterJ");
 letterJ.addEventListener("touchstart", playSound);
-var letterK = document.getElementById("letterK");
+let letterK = document.getElementById("letterK");
 letterK.addEventListener("touchstart", playSound);
-var letterL = document.getElementById("letterL");
+let letterL = document.getElementById("letterL");
 letterL.addEventListener("touchstart", playSound);
-var letterM = document.getElementById("letterM");
+let letterM = document.getElementById("letterM");
 letterM.addEventListener("touchstart", playSound);
-var letterN = document.getElementById("letterN");
+let letterN = document.getElementById("letterN");
 letterN.addEventListener("touchstart", playSound);
-var letterO = document.getElementById("letterO");
+let letterO = document.getElementById("letterO");
 letterO.addEventListener("touchstart", playSound);
-var letterP = document.getElementById("letterP");
+let letterP = document.getElementById("letterP");
 letterP.addEventListener("touchstart", playSound);
-var letterQ = document.getElementById("letterQ");
+let letterQ = document.getElementById("letterQ");
 letterQ.addEventListener("touchstart", playSound);
-var letterR = document.getElementById("letterR");
+let letterR = document.getElementById("letterR");
 letterR.addEventListener("touchstart", playSound);
-var letterS = document.getElementById("letterS");
+let letterS = document.getElementById("letterS");
 letterS.addEventListener("touchstart", playSound);
-var letterT = document.getElementById("letterT");
+let letterT = document.getElementById("letterT");
 letterT.addEventListener("touchstart", playSound);
-var letterU = document.getElementById("letterU");
+let letterU = document.getElementById("letterU");
 letterU.addEventListener("touchstart", playSound);
-var letterV = document.getElementById("letterV");
+let letterV = document.getElementById("letterV");
 letterV.addEventListener("touchstart", playSound);
-var letterW = document.getElementById("letterW");
+let letterW = document.getElementById("letterW");
 letterW.addEventListener("touchstart", playSound);
-var letterX = document.getElementById("letterX");
+let letterX = document.getElementById("letterX");
 letterX.addEventListener("touchstart", playSound);
-var letterY = document.getElementById("letterY");
+let letterY = document.getElementById("letterY");
 letterY.addEventListener("touchstart", playSound);
-var letterZ = document.getElementById("letterZ");
+let letterZ = document.getElementById("letterZ");
 letterZ.addEventListener("touchstart", playSound);
 
 //GETTING THE CLICK EVENT
@@ -134,19 +108,20 @@ letterX.addEventListener("click",playSound);
 letterY.addEventListener("click",playSound);
 letterZ.addEventListener("click",playSound);
 
-//SPELL IT BUTTON CLIC AND TOUCH
-var spellButton = document.getElementById("submitButton");
+//SPELL IT BUTTON CLICK AND TOUCH
+let spellButton = document.getElementById("submitButton");
 spellButton.addEventListener("click", spellSound);
 
 //SPELL SOUND FUNCTION
+
+//timer (for spell function)
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
-async function spellSound(event){
-    var word = document.getElementById("inputText").value;
+async function spellSound() {
+    let word = document.getElementById("inputText").value;
     word = word.toUpperCase();
-    var wlength= word.length;
-    var i;
-    for(i=0;i<wlength;i++) {
+    let wordLength= word.length;
+    for(let i=0; i<wordLength; i++) {
         spellFunction(word.charAt(i));
         console.log(word.charAt(i));
         await timer(1000);
@@ -155,92 +130,14 @@ async function spellSound(event){
 
 //MAIN "PLAYSOUND" FUNCTION
 function playSound (event) {
-    switch(event.target.innerText) {
-        case "A":
-            aSound.play();
-            break;
-        case "B":
-            bSound.play();
-            break;
-        case "C":
-            cSound.play();
-            break;
-        case "D":
-            dSound.play();
-            break;
-        case "E":
-            eSound.play();
-            break;
-        case "F":
-            fSound.play();
-            break;
-        case "G":
-            gSound.play();
-            break;
-        case "H":
-            hSound.play();
-            break;
-        case "I":
-            iSound.play();
-            break;
-        case "J":
-            jSound.play();
-            break;
-        case "K":
-            kSound.play();
-            break;
-        case "L":
-            lSound.play();
-            break;
-        case "M":
-            mSound.play();
-            break;
-        case "N":
-            nSound.play();
-            break;
-        case "O":
-            oSound.play();
-            break;
-        case "P":
-            pSound.play();
-            break;
-        case "Q":
-            qSound.play();
-            break;
-        case "R":
-            rSound.play();
-            break;
-        case "S":
-            sSound.play();
-            break;
-        case "T":
-            tSound.play();
-            break;
-        case "U":
-            uSound.play();
-            break;
-        case "V":
-            vSound.play();
-            break;
-        case "W":
-            wSound.play();
-            break;
-        case "X":
-            xSound.play();
-            break;
-        case "Y":
-            ySound.play();
-            break;
-        case "Z":
-            zSound.play();
-            break;
-        default:
-            console.log("default case");
-            break;
-    }
+    soundMaker(event.target.innerText);
 }
+
 //SPELL "PLAYSOUND" FUNCTION
 function spellFunction (letter) {
+    soundMaker(letter);
+}
+function soundMaker(letter) {
     switch(letter) {
         case "A":
             aSound.play();
@@ -326,9 +223,9 @@ function spellFunction (letter) {
     }
 }
 //sound constructor
-function sound(src){
+function sound(source){
     this.sound = document.createElement("audio");
-    this.sound.src = src;
+    this.sound.src = source;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
